3be99a8bee3b367d7b496e2a4212ef62c0c88dd9
Angel Baez <angelbaez.dev@gmail.com>
2021-04-11 16:14:46 -0500

    config

--- js/init.js
+++ js/init.js
@@ -1,10 +1,17 @@
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyByCBulnwWCdTehc682mt70OCkC08uIAe4",
+  /*apiKey: "AIzaSyByCBulnwWCdTehc682mt70OCkC08uIAe4",
   authDomain: "pwa-angelbz.firebaseapp.com",
   databaseURL: "https://pwa-angelbz-default-rtdb.firebaseio.com",
   projectId: "pwa-angelbz",
   storageBucket: "pwa-angelbz.appspot.com",
   messagingSenderId: "621013189220",
-  appId: "1:621013189220:web:0c96a0d4b9d2bcd9309571"
+  appId: "1:621013189220:web:0c96a0d4b9d2bcd9309571"*/
+  apiKey: "AIzaSyByCBulnwWCdTehc682mt70OCkC08uIAe4",
+    authDomain: "pwa-angelbz.firebaseapp.com",
+    databaseURL: "https://pwa-angelbz-default-rtdb.firebaseio.com",
+    projectId: "pwa-angelbz",
+    storageBucket: "pwa-angelbz.appspot.com",
+    messagingSenderId: "621013189220",
+    appId: "1:621013189220:web:0c96a0d4b9d2bcd9309571"
 });
